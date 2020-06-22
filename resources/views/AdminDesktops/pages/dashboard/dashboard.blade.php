d25be398c06c66aa91ced3302acf53b605b6a7a5
dinhvanlanhit <39582595+dinhvanlanhit@users.noreply.github.com>
2020-06-22 11:48:24 +0700

    lành

--- resources/views/AdminDesktops/pages/dashboard/dashboard.blade.php
+++ resources/views/AdminDesktops/pages/dashboard/dashboard.blade.php
@@ -10,6 +10,12 @@
         ]
         )
     </div>
+    <div class="col-sm-12 col-md-3 ">
+      <div class="form-group">
+        <input class="daterangepicker_label form-control text-center"  value="Tháng này" readonly/>
+      </div>
+  </div>
+    
 </div>
 <div class="row">
   <div class="col-md-12">
