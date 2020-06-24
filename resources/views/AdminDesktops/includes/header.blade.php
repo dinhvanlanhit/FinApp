23c0f386ef229f240525afa358ba47099dcb1cb7
dinhvanlanhit <39582595+dinhvanlanhit@users.noreply.github.com>
2020-06-24 16:41:31 +0700

    a

--- resources/views/AdminDesktops/includes/header.blade.php
+++ resources/views/AdminDesktops/includes/header.blade.php
@@ -1,40 +1,40 @@
 <nav class="main-header navbar navbar-expand navbar-dark navbar-danger">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav ">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item  ">
-        <a href="{{route('wallet')}}" class="nav-link active"> <b> <i class="fa fa-money"></i> </b><b class="surplus" id="surplus"> {{number_format(surplus())}}</b><b> VNĐ </b></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="{{route('profile')}}" class="nav-link active"><b> <i class="fa fa-user"></i> {{Auth::user()->full_name}}</b></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link"><b>Liên Hệ</b></a>
-      </li>
-    </ul>
-    <!-- SEARCH FORM -->
+  <!-- Left navbar links -->
+  <ul class="navbar-nav ">
+    <li class="nav-item">
+      <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+    </li>
+    <li class="nav-item  ">
+      <a href="{{route('wallet')}}" class="nav-link active"> <b> <i class="fa fa-money"></i> </b><b class="surplus" id="surplus"> {{number_format(surplus())}}</b><b> VNĐ </b></a>
+    </li>
+    <li class="nav-item d-none d-sm-inline-block">
+      <a href="{{route('profile')}}" class="nav-link active"><b> <i class="fa fa-user"></i> {{Auth::user()->full_name}}</b></a>
+    </li>
+    <li class="nav-item d-none d-sm-inline-block">
+      <a href="#" class="nav-link"><b>Liên Hệ</b></a>
+    </li>
+  </ul>
+  <!-- SEARCH FORM -->
 
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      {{-- <li class="nav-item">
-        <a href="{{route('mobile-test')}}"  class="nav-link" role="button">
-          <i class="fas fa-mobile-alt"></i>
-        </a>
-      </li> --}}
-      @if (Auth::user()->type=='admin')
-        <li class="nav-item">
-          <a href="{{route('admin_dashboard')}}"  title="Quản Lý Hệ Thống" class="nav-link">
-            <i class="fas fa-desktop"></i>
-          </a>
-        </li>
-      @endif
-      
+  <!-- Right navbar links -->
+  <ul class="navbar-nav ml-auto">
+    {{-- <li class="nav-item">
+      <a href="{{route('mobile-test')}}"  class="nav-link" role="button">
+        <i class="fas fa-mobile-alt"></i>
+      </a>
+    </li> --}}
+    @if (Auth::user()->type=='admin')
       <li class="nav-item">
-        <a href="javascript:void(0)" data-toggle="modal" data-target="#modal-logout" class="nav-link" role="button">
-          <i class="fas fa-sign-out-alt"></i>
+        <a href="{{route('admin_dashboard')}}"  title="Quản Lý Hệ Thống" class="nav-link">
+          <i class="fas fa-desktop"></i>
         </a>
       </li>
-    </ul>
-  </nav>
+    @endif
+    
+    <li class="nav-item">
+      <a href="javascript:void(0)" data-toggle="modal" data-target="#modal-logout" class="nav-link" role="button">
+        <i class="fas fa-sign-out-alt"></i>
+      </a>
+    </li>
+  </ul>
+</nav>
