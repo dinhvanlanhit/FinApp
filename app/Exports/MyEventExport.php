c27bf2f11b31a6c8f760fef3a1d23dd656f33516
dinhvanlanhit <39582595+dinhvanlanhit@users.noreply.github.com>
2020-07-01 16:57:56 +0700

    lành

--- app/Exports/MyEventExport.php
+++ app/Exports/MyEventExport.php
@@ -10,16 +10,48 @@ use App\Exports\Dashboars\EventExport;
 use App\Exports\Dashboars\ShoppingExport;
 use App\Exports\Dashboars\CostExport;
 use App\Exports\Dashboars\SalaryExport;
+use App\Models\GroupMyEvent;
+use App\Models\MyEvent;
+use App\Models\TypeEvent;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 class MyEventExport
 {
     public static function Export($Request)
     {
 
-     
+        $idUser = idUser();
+        $idGroupMyEvent = $Request->input('idGroupMyEvent');
+        $search = $Request->input('search');
+        if(!empty($idGroupMyEvent)){
+            $MyEvent = MyEvent::join('group_my_event','group_my_event.id','=','my_event.idGroupMyEvent')
+            ->where('my_event.idUser','=',$idUser)->where('my_event.idGroupMyEvent','=',$idGroupMyEvent)
+            ->select('my_event.*','group_my_event.group_name')
+            ->Where(function($query)use($search){
+                $query->where('my_event.id', 'LIKE', "%{$search}%")
+                ->orWhere('my_event.name', 'LIKE',"%{$search}%")
+                ->orWhere('my_event.status_name', 'LIKE',"%{$search}%")
+                ->orWhere('my_event.address', 'LIKE',"%{$search}%")
+                ->orWhere('my_event.amount','LIKE',"%{$search}%")
+                ->orWhere('my_event.note','LIKE',"%{$search}%")
+                ->orWhere('my_event.date','LIKE',"%{$search}%");
+            })->get();
+        }else{
+            $MyEvent = MyEvent::join('group_my_event','group_my_event.id','=','my_event.idGroupMyEvent')
+            ->where('my_event.idUser','=',$idUser)
+            ->select('my_event.*','group_my_event.group_name')
+            ->Where(function($query)use($search){
+                $query->where('my_event.id', 'LIKE', "%{$search}%")
+                ->orWhere('my_event.name', 'LIKE',"%{$search}%")
+                ->orWhere('my_event.status_name', 'LIKE',"%{$search}%")
+                ->orWhere('my_event.address', 'LIKE',"%{$search}%")
+                ->orWhere('my_event.amount','LIKE',"%{$search}%")
+                ->orWhere('my_event.note','LIKE',"%{$search}%")
+                ->orWhere('my_event.date','LIKE',"%{$search}%");
+            })->get();
+        }
+        $sumAmount =$MyEvent->sum('amount');
         $spreadsheet = new Spreadsheet();
         $excel = $spreadsheet->getActiveSheet();
-
         $row = 1;
         $excel->getColumnDimension('A')->setWidth(10.5);
         $excel->getColumnDimension('B')->setWidth(15.5);
@@ -28,10 +60,24 @@ class MyEventExport
         $excel->getColumnDimension('E')->setWidth(25.5);
         $excel->getColumnDimension('F')->setWidth(10.5);
         $excel->getColumnDimension('G')->setWidth(50.5);
-
         $excel->setCellValue('A'.$row, 'SỰ KIỆN CỦA TÔI');
         $excel->mergeCells('A'.$row.':G'.$row);
-
+        $row += 1;
+        if(!empty($idGroupMyEvent)){
+            $GroupMyEvent = GroupMyEvent::find($idGroupMyEvent);
+            $excel->setCellValue('A'.$row, 'SỰ KIỆN : '.$GroupMyEvent->group_name);
+            $excel->mergeCells('A'.$row.':G'.$row);
+            $row += 1;
+            $excel->setCellValue('A'.$row, 'NGÀY TỔ CHỨC : '.$GroupMyEvent->date);
+            $excel->mergeCells('A'.$row.':G'.$row);
+            $row += 1;
+            $excel->setCellValue('A'.$row, 'GHI CHÚ : '.$GroupMyEvent->note);
+            $excel->mergeCells('A'.$row.':G'.$row);
+            $row += 1;
+        }
+        $excel->setCellValue('A'.$row, 'TỔNG TIỀN : '.number_format($sumAmount).' VNĐ');
+        $excel->mergeCells('A'.$row.':G'.$row);
+        $row += 1;
         $excel->setCellValue('A'.$row,'STT');
         $excel->setCellValue('B'.$row,'Sự Kiện');
         $excel->setCellValue('C'.$row,'Tên');
@@ -39,7 +85,16 @@ class MyEventExport
         $excel->setCellValue('E'.$row,'Số Tiền');
         $excel->setCellValue('F'.$row,'Ngày');
         $excel->setCellValue('G'.$row,'Ghi Chú');
+        $excel->setCellValue('H'.$row,'Trạng Thái');
         $styleHeaderTitle = array('alignment' => array(
+            'vertical' =>Alignment::VERTICAL_CENTER,
+            'horizontal' =>Alignment::HORIZONTAL_CENTER,
+            'font' => [
+                'bold' => true,
+                'size' =>50
+            ],
+        ));
+        $styleHeaderDate = array('alignment' => array(
             'vertical' =>Alignment::VERTICAL_CENTER,
             'horizontal' =>Alignment::HORIZONTAL_CENTER,
             'font' => [
@@ -49,13 +104,13 @@ class MyEventExport
         ));
         $excel->getStyle('A'.$row.':G'.$row)->applyFromArray($styleHeaderTitle);
         $stt = 0;
-        foreach($event as $item){
+        foreach($MyEvent as $item){
             $row++;
             $stt++;
             $excel->setCellValue('A'.$row,$stt);
             $excel->getStyle('A'.$row)->applyFromArray($styleHeaderDate);
             $excel->getStyle('A'.$row)->getAlignment()->setWrapText(true);
-            $excel->setCellValue('B'.$row,$item->type_name);
+            $excel->setCellValue('B'.$row,$item->group_name);
             $excel->getStyle('B'.$row)->getAlignment()->setWrapText(true);
             $excel->setCellValue('C'.$row,$item->name);
             $excel->getStyle('C'.$row)->getAlignment()->setWrapText(true);
@@ -69,6 +124,9 @@ class MyEventExport
             $excel->setCellValue('G'.$row,$item->note);
             $excel->getStyle('G'.$row)->getAlignment()->setWrapText(true);
             $excel->getStyle('G'.$row)->applyFromArray($styleHeaderDate);
+            $excel->setCellValue('H'.$row,$item->status_name);
+            $excel->getStyle('H'.$row)->applyFromArray($styleHeaderDate);
+            $excel->getStyle('H'.$row)->getAlignment()->setWrapText(true);
         }
         $row+=1;
         $excel->setCellValue('E'.$row,'Tổng Tiền : '.number_format($sumAmount).' VNĐ');
